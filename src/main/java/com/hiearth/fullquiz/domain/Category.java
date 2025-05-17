cb88d193472622a6a07ff2f4325eae87525e0cee
박승범 <seoungbeom@bagseungbeom-ui-MacBookAir.local>
2025-05-17 16:56:48 +0900

    Entity Design

---
+++ src/main/java/com/hiearth/fullquiz/domain/Category.java
@@ -0,0 +1,28 @@
+package com.hiearth.fullquiz.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Category extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Category> children = new ArrayList<>();
+}
