52d5ff90e2addb4af0dc4ac130f8936045b9bfdd
5ooyeon <melthetensai@gmail.com>
2025-05-18 07:21:18 +0900

    Refactor: 닉네임 길이 제한 추가

--- src/main/java/com/hiearth/fullquiz/service/request/MemberCreateDTO.java
+++ src/main/java/com/hiearth/fullquiz/service/request/MemberCreateDTO.java
@@ -1,8 +1,13 @@
 package com.hiearth.fullquiz.service.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class MemberCreateDTO {
+
+    @NotNull
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
     private String nickname;
 }
