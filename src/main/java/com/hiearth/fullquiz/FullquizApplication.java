217312080908d4ffca8832aaa10d700d733725b9
5ooyeon <melthetensai@gmail.com>
2025-05-17 15:08:27 +0900

    init

---
+++ src/main/java/com/hiearth/fullquiz/FullquizApplication.java
@@ -0,0 +1,13 @@
+package com.hiearth.fullquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiearth/fullquiz/FullquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiearth.fullquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
