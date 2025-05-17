05b2dd40f387d29f67a0aa87dafddbd832d43346
dbalsk <dbalsrltk@naver.com>
2025-05-17 21:57:44 +0900

    Feat: member의 nickname 체크 및 회원가입 로직 구현

--- src/main/java/com/hiearth/fullquiz/repository/CategoryRepository.java
+++ src/main/java/com/hiearth/fullquiz/repository/CategoryRepository.java
@@ -4,11 +4,17 @@ import com.hiearth.fullquiz.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByName(String name);
 
+    default List<Category> getAllCategories() {
+        return findAll();
+    }
 }
